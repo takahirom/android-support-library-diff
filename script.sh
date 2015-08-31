44afd2c7e5d68d580f6b6e9616876c00034804c0
takahirom <takam.dev@gmail.com>
2015-09-01 00:57:55 +0900

    first commit

---
+++ script.sh
@@ -0,0 +1,10 @@
+cd $1
+ls -F | grep /|awk -v arg=$1 'BEGIN{sub("/","",arg);libraryPrefix=arg}
+NR%2==1 {a=$0;sub("/","",a)}
+NR%2==0 {b=$1;sub("/","",b)}
+NR%2==0 {;print "jardiff.py " libraryPrefix "/" a "/" libraryPrefix "-" a".aar " libraryPrefix "/" b "/" libraryPrefix "-" b ".aar >" libraryPrefix "/" a"-"b".patch\ncp " libraryPrefix"/"a"-"b".patch ~/git/android-support-library-diff/" libraryPrefix "/"}
+NR%2==1 && length(b) != 0 {print "jardiff.py " libraryPrefix "/" b "/" libraryPrefix "-" b".aar " libraryPrefix "/" a "/" libraryPrefix "-" a ".aar >" libraryPrefix "/" b"-"a".patch\ncp " libraryPrefix"/"b"-"a".patch ~/git/android-support-library-diff/" libraryPrefix "/"}
+'>../cp.sh
+cd ..
+cat cp.sh
+sh cp.sh
