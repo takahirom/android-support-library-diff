dff7f02b351e7cf6d956b7becbdf94641f8f7381
takahirom <takam.dev@gmail.com>
2017-03-25 00:10:36 +0900

    make no generate diff for old versions

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ NR%2==1 {a=$0;sub("/","",a)}
 NR%2==0 {b=$1;sub("/","",b)}
 NR%2==0 {;print "jardiff.py " libraryPrefix "/" a "/" libraryPrefix "-" a".aar " libraryPrefix "/" b "/" libraryPrefix "-" b ".aar >" libraryPrefix "/" a"-"b".patch\ncp -n " libraryPrefix"/"a"-"b".patch ~/git/android-support-library-diff/" libraryPrefix "/"}
 NR%2==1 && length(b) != 0 {print "jardiff.py " libraryPrefix "/" b "/" libraryPrefix "-" b".aar " libraryPrefix "/" a "/" libraryPrefix "-" a ".aar >" libraryPrefix "/" b"-"a".patch\ncp -n " libraryPrefix"/"b"-"a".patch ~/git/android-support-library-diff/" libraryPrefix "/"}
-'>../cp.sh
+'|grep -v "18."|grep -v "19."|grep -v "20."|grep -v "21."|grep -v "22."|grep -v "23."|grep -v "24.">../cp.sh
 cd ..
 cat cp.sh
 sh cp.sh
